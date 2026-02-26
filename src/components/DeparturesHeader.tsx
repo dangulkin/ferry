887926d8b99e4861f4294552b67498b43df836eb
Dan Gulkin <d.gulyaikin@gmail.com>
2026-02-26 15:59:47 +0000

    Direction choice added

--- src/components/DeparturesHeader.tsx
+++ src/components/DeparturesHeader.tsx
@@ -13,14 +13,14 @@ const DeparturesHeader: React.FC<DeparturesHeaderProps> = ({ isSpecificRoute, ro
 
 	return (
 		<header className="pt-safe bg-gray-50 sticky top-0 z-10">
-			<div className="pt-4 min-h-[98px] py-2 flex items-center">
+			<div className="pt-4 h-[98px] py-2 flex items-center">
 				<button
 					onClick={onBack}
 					className="size-10 mx-3 shrink-0 flex items-center justify-center text-gray-900 hover:bg-gray-200 rounded-full"
 				>
 					<LighthouseIcon size={24} weight="bold" />
 				</button>
-				<div className="flex flex-1 items-center">
+				<div className="flex flex-1 items-center h-full">
 					{isSpecificRoute && currentRoute ? (
 						<div className="flex items-center justify-between flex-1">
 							<div className={`flex flex-col border-l-6 ${currentRoute.borderColor} pl-3 py-1`}>
